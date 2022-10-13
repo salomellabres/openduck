dc5ccbeb20b9821208f733c7a19bbb4005766c76
seorito <alvaro.serrano.morras@gmail.com>
2022-10-13 08:36:58 +0100

    corrected HMR from single preparation script

--- scripts/duck_prepare_sys_for_amber.py
+++ scripts/duck_prepare_sys_for_amber.py
@@ -12,15 +12,10 @@ except ModuleNotFoundError:
 def main():
     parser = argparse.ArgumentParser(description='Prepare system for dynamic undocking')
     parser.add_argument('-p', '--protein', help='chunk protein in PDB format')
-    parser.add_argument('-l', '--ligand', help='Ligand in mol format')
-    # parser.add_argument('-o', '--output', help="PDB output")
-    #parser.add_argument('-c', '--chunk', help='Chunked protein')
+    parser.add_argument('-l', '--ligands', help='Ligands in sdf format')
     parser.add_argument('-i', '--interaction', help='Protein atom to use for ligand interaction.')
-    #parser.add_argument('-s', '--seed', type=int, help='Random seed.')
-    #parser.add_argument('--gpu-id', type=int, help='GPU ID (optional); if not specified, runs on CPU only.')
-    #parser.add_argument('--force-constant-eq', type=float, default=1.0, help='Force constant for equilibration.')
-    parser.add_argument('-q','--queue-template', type=str, default = None, help='Write out a queue template from the following: [Slurm | SGE]')
-    parser.add_argument('--HMR', type=bool, default=True, help ='Perform Hydrogen Mass Repartition on the topology and use it for the input files')
+    parser.add_argument('-q', '--queue-template', type=str, default = None, help='Write out a queue template from the following: [Slurm | SGE]')
+    parser.add_argument('-H','--HMR', action='store_true', help ='Perform Hydrogen Mass Repartition on the topology and use it for the input files')
     parser.add_argument('-r', '--replicas', type=int, default=5, help='Ammount of SMD replicas to perform')
     parser.add_argument('-w', '--wqb_threshold', type=float, default=7.0, help='WQB threshold to stop the simulations')
     args = parser.parse_args()
@@ -44,7 +39,7 @@ def main():
     write_all_inputs(p[0], p[1:], hmr = args.HMR)
 
     if args.queue_template:
-        write_queue_template(args.queue_template, hmr = args.HMR, replicas=replicas, wqb_threshold=wqb_threshold)
+        write_queue_template(args.queue_template, hmr = args.HMR, replicas=args.replicas, wqb_threshold=args.wqb_threshold)
     
 
 
